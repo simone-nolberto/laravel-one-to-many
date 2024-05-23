c9a679fb1400a724fd77e417fcdec6dfce8777cd
simone-nolberto <simone.nolberto@outlook.com>
2024-05-23 13:00:26 +0200

    update with fix for type and project model

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,11 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Type;
 
 class Project extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['author', 'project_title', 'slug', 'description', 'cover_image', 'source_code', 'site_link'];
+    protected $fillable = ['author', 'project_title', 'slug', 'description', 'cover_image', 'source_code', 'site_link', 'type_id'];
 
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
